72e8f6ef14a7a9c8c6442189bc7cbb4948dfa47d
MahdiGMK <mbahramiand@gmail.com>
2023-05-20 00:49:18 +0330

    initial commit

---
+++ core/src/com/mahdigmk/apaa/AAGame.java
@@ -0,0 +1,26 @@
+package com.mahdigmk.apaa;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import static com.badlogic.gdx.Gdx.*;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+
+public class AAGame extends Game {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0.75f, 0.75f, 0.75f, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+
+}
